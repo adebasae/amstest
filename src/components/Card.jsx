46291109e5d083d3d4bc76b00e3ec468c2e51d3c
Adrian Debasa <debasa1990@gmail.com>
2022-01-28 21:56:13 -0500

    prop types

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,43 +4,54 @@ import PropTypes from 'prop-types';
 function Card(props) {
   const { imagen, description, marca, modelo, precio } = props;
   return (
-    <div className="container card-container d-flex ">
-      <div className="card p-2">
-        <div className="p-info px-3 py-3">
-          <div>
-            <h5 className="mb-0">{modelo}</h5>
-            <span>{marca}</span>
-          </div>
-          <div className="p-price d-flex flex-row">
-            <span>$</span>
-            <h1>{precio}</h1>
-          </div>
-          <div className="heart">
-            <i className="bx bx-heart" />
-          </div>
+    <div className="card p-2">
+      <div className="p-info px-3 py-3">
+        <div>
+          <h5 className="mb-0">{modelo}</h5>
+          <span>{marca}</span>
         </div>
-        <div className="text-center p-image">
-          <img alt={imagen.alt} src={imagen.src} />
+        <div className="p-price d-flex flex-row">
+          <span>$</span>
+          <h1>{precio}</h1>
         </div>
-        <div className="p-about">
-          <p>{description}</p>
+        <div className="heart">
+          <i className="bx bx-heart" />
         </div>
-        <div className="buttons d-flex flex-row gap-3 px-3 w-75">
-          <button type="button" className="btn btn-outline-danger w-100">
-            Comprar
-          </button>
-        </div>
+      </div>
+      <div className="text-center p-image">
+        <img alt={imagen.alt} src={imagen.src} />
+      </div>
+      <div className="p-about">
+        <p>{description}</p>
+      </div>
+      <div className="buttons d-flex flex-row gap-3 px-3 w-75">
+        <button type="button" className="btn btn-outline-danger w-100">
+          Comprar
+        </button>
       </div>
     </div>
   );
 }
 
 Card.propTypes = {
-  imagen: PropTypes.object,
-  description: PropTypes.string,
-  marca: PropTypes.string,
-  modelo: PropTypes.string,
-  precio: PropTypes.number
+  imagen: PropTypes.shape({
+    alt: PropTypes.string.isRequired,
+    src: PropTypes.string.isRequired
+  }).isRequired,
+  description: PropTypes.string.isRequired,
+  marca: PropTypes.string.isRequired,
+  modelo: PropTypes.string.isRequired,
+  precio: PropTypes.number.isRequired
 };
+
+// // Specifies the default values for props:
+// Card.defaultProps = {
+//   imagen: { alt: 'imgtest', src: 'https://i.imgur.com/hpftqCo.png' },
+//   description:
+//     'Lorem ipsum es el texto que se usa habitualmente en diseño gráfico en demostraciones de tipografías o de borradores de diseño para probar el diseño visual antes de insertar el texto final',
+//   modelo: 'Professional Headphones',
+//   marca: 'Beats By Dre',
+//   precio: 299
+// };
 
 export default Card;
--- src/pages/ListaProducto.jsx
+++ src/pages/ListaProducto.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import ProductService from '../services/ProductService';
+import Card from '../components/Card';
 
 function ListProduct() {
   const [products, setProducts] = useState([]);
@@ -8,8 +9,23 @@ function ListProduct() {
       setProducts(res.data);
     });
   }, []);
-  const percent = ` Lista de productos ${products.length} `;
-  return <div> {percent}</div>;
+
+  const listaProducto = () =>
+    products.length === 0
+      ? 'poner spinner'
+      : products.map(({ imagen, description, marca, modelo, precio, id }) => (
+          <Card
+            key={id}
+            imagen={imagen}
+            marca={marca}
+            description={description}
+            modelo={modelo}
+            precio={precio}
+          />
+        ));
+  return (
+    <div className="container card-container d-flex ">{listaProducto()}</div>
+  );
 }
 
 export default ListProduct;
