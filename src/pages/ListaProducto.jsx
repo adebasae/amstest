a96324671edd1d324ee8b8ea3b4c7c80aba92e71
Adrian Debasa <debasa1990@gmail.com>
2022-01-29 16:08:02 -0500

    product list

--- src/pages/ListaProducto.jsx
+++ src/pages/ListaProducto.jsx
@@ -5,41 +5,22 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import ProductService from '../services/ProductService';
 import Card from '../components/Card';
-import Pagination from '../components/Pagination';
+// import Pagination from '../components/Pagination';
 import isEmpty from '../components/utils';
 
 function ListProduct() {
   const [products, setProducts] = useState([]);
-  // const [totalProducts, setTotalProducts] = useState();
-  const [pageOfItems, setPageOfItems] = useState([]);
 
-  // const [paginationSetting, setPaginationSetting] = useState({
-  //   currentProducts: [],
-  //   currentPage: null,
-  //   totalPages: null
-  // });
   useEffect(() => {
     ProductService.getAllProducts().then((res) => {
       setProducts(res.data);
     });
   }, []);
 
-  // const onPageChanged = (data) => {
-  //   const { currentPage, totalPages, pageLimit } = data;
-  //   const offset = (currentPage - 1) * pageLimit;
-  //   const currentProducts = products.slice(offset, offset + pageLimit);
-  //   setPaginationSetting({ currentProducts, currentPage, totalPages });
+  // const onChangePage = (pageOfItems) => {
+  //   // update state with new page of items
+  //   this.setState({ pageOfItems: pageOfItems });
   // };
-
-  const onChangePage = (pageOfItems) => {
-    // update state with new page of items
-    this.setState({ pageOfItems: pageOfItems });
-  };
-
-  // useEffect(() => {
-  //   console.log('products', products);
-  //   setTotalProducts(products.length);
-  // }, [products]);
 
   const filter = (text) => {
     ProductService.getAllProducts().then((res) => {
@@ -60,21 +41,19 @@ function ListProduct() {
   };
 
   const productList = () =>
-    paginationSetting.currentProducts.length === 0 ? (
+    products.length === 0 ? (
       <p>poner spinner</p>
     ) : (
-      paginationSetting.currentProducts.map(
-        ({ imagen, description, marca, modelo, precio, id }) => (
-          <Card
-            key={id}
-            imagen={imagen}
-            marca={marca}
-            description={description}
-            modelo={modelo}
-            precio={precio}
-          />
-        )
-      )
+      products.map(({ imagen, description, marca, modelo, precio, id }) => (
+        <Card
+          key={id}
+          imagen={imagen}
+          marca={marca}
+          description={description}
+          modelo={modelo}
+          precio={precio}
+        />
+      ))
     );
   return (
     <div className="container card-container d-flex ">
@@ -104,7 +83,7 @@ function ListProduct() {
           {productList()}
           <div className="w-100 px-4 py-5 d-flex flex-row flex-wrap align-items-center justify-content-between">
             <div className="d-flex flex-row py-4 align-items-center mx-auto">
-              <Pagination items={products} onChangePage={onChangePage} />
+              {/* <Pagination items={products} onChangePage={onChangePage} /> */}
             </div>
           </div>
         </>
