09f3d6518e9c5b13684421bd862f582266a2be2a
Adrian Debasa <debasa1990@gmail.com>
2022-01-30 20:17:27 -0500

    refactoring product service

--- src/services/ProductService.jsx
+++ src/services/ProductService.jsx
@@ -1,54 +1,11 @@
 import { trackPromise } from 'react-promise-tracker';
-import Cookies from 'universal-cookie';
-
-import api from '../api/api';
-
-const SECOND = 60;
-const MINUTES = 1;
-const TIME_COOKIE = SECOND * MINUTES;
+import { sendServer } from './utils';
 
 const ProductService = {
-  getAllProducts: () => {
-    const cookies = new Cookies();
-    const allProductCookie = cookies.get('allProductCookie');
-
-    if (allProductCookie !== undefined) {
-      const promise = new Promise((resolve) => {
-        resolve(allProductCookie);
-      });
-      return trackPromise(promise);
-    }
-
-    return trackPromise(
-      api.get(`/product`, {}).then((res) => {
-        cookies.set('allProductCookie', res, {
-          path: '/',
-          maxAge: TIME_COOKIE
-        });
-        return res;
-      })
-    );
-  },
-  getProductById: (id) => {
-    const cookies = new Cookies();
-    const productDetail = cookies.get('productDetail');
-
-    if (productDetail !== undefined) {
-      const promise = new Promise((resolve) => {
-        resolve(productDetail);
-      });
-      return trackPromise(promise);
-    }
-
-    return trackPromise(
-      api.get(`/product/${id}`, {}).then((res) => {
-        cookies.set('productDetail', res, {
-          path: '/',
-          maxAge: TIME_COOKIE
-        });
-        return res;
-      })
-    );
-  }
+  getAllProducts: () =>
+    trackPromise(sendServer('allProductCookie', '/product')),
+  getProductById: (id) =>
+    trackPromise(sendServer('productDetail', `/product/${id}`))
 };
+
 export default ProductService;
