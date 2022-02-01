2a41ec11c71d5d87dd3b3e970777d9085d2a14f3
Adrian Debasa <debasa1990@gmail.com>
2022-01-31 20:06:16 -0500

    Breadcrumbs

--- src/components/Breadcumbs.jsx
+++ src/components/Breadcumbs.jsx
@@ -1,29 +1,13 @@
 import React from 'react';
 import { Route, Link } from 'react-router-dom';
 
-function crumb(part, partIndex, parts) {
-  const path = ['', ...parts.slice(0, partIndex + 1)].join('/');
-  return (
-    <Link key={path} to={path}>
-      {part}
-    </Link>
-  );
-}
-
 function Breadcrumbs() {
   return (
     <Route
       path="*"
-      render={(props) => {
-        let parts = props.location.pathname.split('/');
-        const place = parts[parts.length - 1];
-        parts = parts.slice(1, parts.length);
-        return (
-          <p>
-            {parts.map(crumb)}/{place}
-          </p>
-        );
-      }}
+      render={(props) => (
+        <Link to={props.location.pathname}>{props.location.pathname}</Link>
+      )}
     />
   );
 }
